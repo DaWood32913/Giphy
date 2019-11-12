e3011e0481cbb795f5a0fd2057a03d0b13c2e19d
DaWood32913 <woodleyjohnson@yahoo.com>
2019-11-12 16:25:41 -0500

    setup animal array

---
+++ giphy.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    var animals = ["dog", "cat","rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+
+    
+})
