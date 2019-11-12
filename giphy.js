f8e2ec5b4dff9910ea1a1324742d10f3d103eeab
DaWood32913 <woodleyjohnson@yahoo.com>
2019-11-12 16:35:51 -0500

    created function to append buttons

--- giphy.js
+++ giphy.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
 
     var animals = ["dog", "cat","rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 
-    
+    // function to make buttons and add to page
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i = 0; i < arrayToUse.length; i++) {
+            var a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
+        }
+    }
 })
